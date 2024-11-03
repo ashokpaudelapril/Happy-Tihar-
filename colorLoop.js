5d34d0be718987a8d8f8eb618bdadfe268be1c70
Ashok Paudel <ashokpaudelapril@gmail.com>
2024-11-02 23:52:32 -0500

    Implement Code for better functioning

--- colorLoop.js
+++ colorLoop.js
@@ -14,35 +14,19 @@ function colorChange(bgColor, textColor, delay) {
     });
 }
 
-function startColorLoop() {
-    colorChange("#FFD700", "#510060", 1000)
-        .then(() => {
-            console.log("Red was completed");
-            return colorChange("#FF4500", "#790053", 1000);
-        })
-        .then(() => {
-            console.log("Orange was completed");
-            return colorChange("#2C3E50", "#C6095D", 1000);
-        })
-        .then(() => {
-            console.log("Yellow was completed");
-            return colorChange("#F39C12", "#F7CD3F", 1000);
-        })
-        .then(() => {
-            console.log("Blue was completed");
-            return colorChange("#6A1B9A", "#FD7E21", 1000);
-        })
-        .then(() => {
-            console.log("Pink was completed");
-            return colorChange("#FFE4B5", "#0F7926", 1000);
-        })
-        .then(() => {
-            console.log("Green was completed");
-            startColorLoop();  // Call the function again to start over
-        })
-        .catch(() => {
-            console.log("Color was not changed");
-        });
+function getRandomColor() {
+  // Generate a random hexadecimal color code
+  let letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+async function startColorLoop() {
+    await colorChange(getRandomColor(), getRandomColor(), 1000);
+    await startColorLoop();
 }
 
 // Start the loop
